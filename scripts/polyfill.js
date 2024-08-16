f0241b4da69ffb3c15e781b224978098a74e17fb
Kai <anhchang1412@gmail.com>
2024-08-16 15:59:09 +0700

    Fix: old widget logic

--- scripts/polyfill.js
+++ scripts/polyfill.js
@@ -5,3 +5,21 @@ Array.prototype.map = function (callback) {
   }
   return temp;
 };
+
+if (!Object.values) {
+  Object.values = function (obj) {
+    if (obj !== Object(obj)) {
+      throw new TypeError('Object.values called on a non-object');
+    }
+
+    var values = [];
+
+    for (var key in obj) {
+      if (Object.prototype.hasOwnProperty.call(obj, key)) {
+        values.push(obj[key]);
+      }
+    }
+
+    return values;
+  };
+}
