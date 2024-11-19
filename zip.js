c1b790e3e59022708ed95257c10d93c32c46e380
Kai <anhchang1412@gmail.com>
2024-11-20 03:54:53 +0700

    ASDADS

--- zip.js
+++ zip.js
@@ -2,8 +2,9 @@
 const fs = require('fs');
 const archiver = require('archiver');
 
+const version = 'v0.0.1';
 var distDir = process.cwd() + '/dist';
-var output = fs.createWriteStream(distDir + '/menuzen-widget.zip');
+var output = fs.createWriteStream(distDir + `/menuzen-widget-${version}.zip`);
 var archive = archiver('zip');
 if (!fs.existsSync(distDir)) {
   fs.mkdirSync(distDir, 0744);
